80f3e13b7467a6e857f6faac61a0da399a8d7914
KoTTiG <d.gasilov@mail.ru>
2023-04-12 11:30:20 +0300

    #4 docker realisation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import argparse
-import serverPart
+import server
 import node
 from gevent import monkey
 
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     pars.add_argument('server_id', nargs='?')
     args = pars.parse_args().server_id
     current_node = node.Node(int(args))
-    serverPart.start(int(args), current_node)
+    server.start(int(args), current_node)
